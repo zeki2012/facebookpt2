d57f07ed391152c0b34d843671e339ea39d94a1b
= <=>
2023-10-23 17:38:37 +0400

    first commit

---
+++ test.js
@@ -0,0 +1,6 @@
+let yeni = prompt('Yeni acacaginiz hesabin adini yazin!');
+let sifre = prompt('Yeni acacaginiz hesabin sifresini yazin!');
+
+let netice = `Zekrota xosgelmisiniz ${yeni}!`;
+
+document.getElementById('new').innerHTML = netice
